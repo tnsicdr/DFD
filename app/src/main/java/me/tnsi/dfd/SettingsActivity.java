5adc2d464938805e7b90e7a0f96012ad3ba62c18
Kevin Harrison <kharrison2797@gmail.com>
2016-11-18 01:43:02 -0500

    Added blank  activities for nav bar buttons and linked them to the buttons. Also added click listener for each map location.

---
+++ app/src/main/java/me/tnsi/dfd/SettingsActivity.java
@@ -0,0 +1,13 @@
+package me.tnsi.dfd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
